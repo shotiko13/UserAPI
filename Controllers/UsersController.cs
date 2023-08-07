ad335103eb4e6b67e9a88a08adce6d9f0a25cde0
k3rnel_panic <shotov1303@gmail.com>
2023-08-07 23:52:37 +0400

    refactored controller code, made it look good

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,129 +1,154 @@
-﻿using Hw4.Data;
+﻿using System.Security.Claims;
+using System.Web.Mvc;
+using Hw4.Data;
 using Hw4.Models;
 using Hw4.Models.UserViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using ControllerBase = Microsoft.AspNetCore.Mvc.ControllerBase;
+using ModelStateDictionary = Microsoft.AspNetCore.Mvc.ModelBinding.ModelStateDictionary;
 
 namespace Hw4.Controllers
 {
-    [Route("api/[controller]")]
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
     [ApiController]
     public class UsersController : ControllerBase
     {
         private readonly UserManager<ApplicationUser> _userManager;
-        private readonly ApplicationDbContext _context;
         private readonly SignInManager<ApplicationUser> _signInManager;
-
         public UsersController(UserManager<ApplicationUser> userManager, ApplicationDbContext context, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
-            _context = context;
             _signInManager = signInManager;
         }
 
-        [HttpPost("register")]
+        [Microsoft.AspNetCore.Mvc.HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterViewModel model)
         {
-            if (!ModelState.IsValid)
-            {
-                var errors = ModelState.SelectMany(x => x.Value.Errors.Select(e => e.ErrorMessage)).ToList();
-                return BadRequest(new { Errors = errors });
-            }
+            var modelStateCheck = CheckModelState(ModelState);
+            if (!modelStateCheck.IsValid)
+                return BadRequest(new { Errors = modelStateCheck.Errors });
+            var user = CreateNewUser(model);
+            var result = await _userManager.CreateAsync(user, model.Password);
+            return await ProcessCreationResult(result, user);
+        }
 
-            var user = new ApplicationUser
+        private ModelCheckResult CheckModelState(ModelStateDictionary modelState)
+        {
+            if (modelState.IsValid)
+                return new ModelCheckResult { IsValid = true };
+            var errors = ModelState
+                .SelectMany(x => x.Value.Errors.Select(e => e.ErrorMessage))
+                .ToList();
+            return new ModelCheckResult { IsValid = false, Errors = errors };
+        }
+
+        private ApplicationUser CreateNewUser(RegisterViewModel model)
+        {
+            return new ApplicationUser
             {
                 UserName = model.Name,
                 Email = model.EmailAddress,
                 RegistrationTime = DateTime.Now,
             };
-
-            var result = await _userManager.CreateAsync(user, model.Password);
-
-            if (result.Succeeded)
-            {
-                user.Status = UserStatus.Active;
-                var updateResult = await _userManager.UpdateAsync(user);
-
-                if (!updateResult.Succeeded)
-                {
-                    var identityErrors = updateResult.Errors.Select(e => e.Description).ToList();
-                    return BadRequest(new { Errors = identityErrors });
-                }
-                return Ok(new { Message = "User created successfully", UserId = user.Id });
-            }
-            else
-            {
-                var identityErrors = result.Errors.Select(e => e.Description).ToList();
-                return BadRequest(new { Errors = identityErrors });
-            }
-
         }
 
-        [HttpPost("login")]
+        private async Task<IActionResult> ProcessCreationResult(IdentityResult result, ApplicationUser user)
+        {
+            if (result.Succeeded)
+                return await UpdateUserStatus(user);
+            return GetErrorResult(result);
+        }
+
+        private async Task<IActionResult> UpdateUserStatus(ApplicationUser user)
+        {
+            user.Status = UserStatus.Active;
+            var updateResult = await _userManager.UpdateAsync(user);
+            if (updateResult.Succeeded)
+                return Ok(new { Message = "User created successfully", UserId = user.Id });
+            return GetErrorResult(updateResult);
+        }
+
+        private IActionResult GetErrorResult(IdentityResult result)
+        {
+            var identityErrors = result.Errors.Select(e => e.Description).ToList();
+            return BadRequest(new { Errors = identityErrors });
+        }
+
+        private class ModelCheckResult
+        {
+            public bool IsValid { get; set; }
+            public List<string> Errors { get; set; }
+        }
+
+
+        [Microsoft.AspNetCore.Mvc.HttpPost("login")]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            ApplicationUser user;
-            if (model.EmailOrUsername.Contains('@'))
-            {
-                user = await _userManager.FindByEmailAsync(model.EmailOrUsername);
-            }
-            else
-            {
-                user = await _userManager.FindByNameAsync(model.EmailOrUsername);
-            }
-
+            var modelStateCheck = CheckModelState(ModelState);
+            if (!modelStateCheck.IsValid)
+                return BadRequest(new { Errors = modelStateCheck.Errors });
+            var user = await FindUser(model.EmailOrUsername);
             if (user == null)
-            {
                 return BadRequest(new { message = "Username or password is incorrect" });
-            }
-
             if (user.Status == UserStatus.Blocked)
-            {
-                return StatusCode(403, new { message = "You are blocked" });
-            }
+                return StatusCode(403, new { message = "Your account is blocked" });
+            return await ProcessSignIn(user, model.Password);
+        }
 
-            if (user != null)
-            {
-                var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
-                if (result.Succeeded)
-                {
-                    user.LastLoginTime = DateTime.Now;
+        private async Task<ApplicationUser> FindUser(string emailOrUsername)
+        {
+            if (emailOrUsername.Contains('@'))
+                return await _userManager.FindByEmailAsync(emailOrUsername);
+            return await _userManager.FindByNameAsync(emailOrUsername);
+        }
 
-                    var updateResult = await _userManager.UpdateAsync(user);
-
-                    if (!updateResult.Succeeded)
-                    {
-                        var identityErrors = updateResult.Errors.Select(e => e.Description).ToList();
-                        return BadRequest(new { Errors = identityErrors });
-                    }
-
-                    await _signInManager.SignInAsync(user, isPersistent: false);
-
-                    return Ok();
-                }
-            }
-
+        private async Task<IActionResult> ProcessSignIn(ApplicationUser user, string password)
+        {
+            var result = await _signInManager.CheckPasswordSignInAsync(user, password, false);
+            if (result.Succeeded) return await UpdateLastLoginTimeAndSignIn(user);
             ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-
             return BadRequest(ModelState);
         }
 
+        private async Task<IActionResult> UpdateLastLoginTimeAndSignIn(ApplicationUser user)
+        {
+            user.LastLoginTime = DateTime.Now;
+            var updateResult = await _userManager.UpdateAsync(user);
+            if (!updateResult.Succeeded)
+                return GetErrorResult(updateResult);
+            await _signInManager.SignInAsync(user, isPersistent: false);
+            return Ok(new { Message = "Login successful" });
+        }
 
-        [HttpGet("{id}")]
+        
+        [Microsoft.AspNetCore.Mvc.HttpGet("{id}")]
         public async Task<IActionResult> GetUser(string id)
         {
             var user = await _userManager.FindByIdAsync(id);
-
             if (user == null)
             {
                 return NotFound();
             }
-            var userDetailViewModel = new UserDetailViewModel
+
+            var userDetailViewModel = CreateUserDetailViewModel(user);
+            return Ok(userDetailViewModel);
+        }
+
+        [Microsoft.AspNetCore.Mvc.HttpGet]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            var users = await _userManager.Users.ToListAsync();
+
+            var userDetailViewModels = users.Select(user => CreateUserDetailViewModel(user)).ToList();
+
+            return Ok(userDetailViewModels);
+        }
+
+        private UserDetailViewModel CreateUserDetailViewModel(ApplicationUser user)
+        {
+            return new UserDetailViewModel
             {
                 Id = user.Id,
                 Name = user.UserName,
@@ -132,90 +157,45 @@ namespace Hw4.Controllers
                 LastLoginTime = user.LastLoginTime,
                 Status = user.Status
             };
-
-            return Ok(userDetailViewModel);
         }
 
-        [HttpGet]
-        public async Task<IActionResult> GetAllUsers()
-        {
-            var users = await _userManager.Users.ToListAsync();
 
-            var userDetailViewModels = users.Select(user => new UserDetailViewModel
-            {
-                Id = user.Id,
-                Name = user.UserName,
-                Email = user.Email,
-                LastLoginTime = user.LastLoginTime,
-                RegistrationTime = user.RegistrationTime,
-                Status = user.Status,
-            }).ToList();
-
-            return Ok(userDetailViewModels);
-        }
-
-        [HttpPost("block/{id}")]
+        [Microsoft.AspNetCore.Mvc.HttpPost("block/{id}")]
         public async Task<IActionResult> BlockUser(string id)
         {
             var user = await _userManager.FindByIdAsync(id);
-
             if (user == null)
-            {
                 return NotFound();
-            }
-
             user.Status = UserStatus.Blocked;
-
             var result = await _userManager.UpdateAsync(user);
-
             if (result.Succeeded)
-            {
                 return Ok(user);
-            }
-
             var identityErrors = result.Errors.Select(e => e.Description).ToList();
             return BadRequest(new { Errors = identityErrors });
         }
 
-        [HttpPost("unblock/{id}")]
+        [Microsoft.AspNetCore.Mvc.HttpPost("unblock/{id}")]
         public async Task<IActionResult> UnblockUser(string id)
         {
             var user = await _userManager.FindByIdAsync(id);
-
             if (user == null)
-            {
                 return NotFound();
-            }
-
             user.Status = UserStatus.Active;
-
             var result = await _userManager.UpdateAsync(user);
-
             if (result.Succeeded)
-            {
                 return Ok();
-            }
-
             return BadRequest(result.Errors);
         }
 
-        [HttpPost("delete/{id}")]
+        [Microsoft.AspNetCore.Mvc.HttpPost("delete/{id}")]
         public async Task<IActionResult> DeleteUser(string id)
         {
             var user = await _userManager.FindByIdAsync(id);
-
             if (user == null)
-            {
                 return NotFound();
-            }
-
             var result = await _userManager.DeleteAsync(user);
-
             if (result.Succeeded)
-            {
                 return Ok(new { Message = "User deleted successfully" });
-            }
-
             var identityErrors = result.Errors.Select(e => e.Description).ToList();
             return BadRequest(new { Errors = identityErrors });
         }
